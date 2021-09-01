7844733c8d8f4e5301b9fcc59a0559d6720d21fa
Pauline122112 <pauline.design007@gmail.com>
2021-09-01 14:36:56 -0500

    validating middleware.js

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,24 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log('this is happening')
+  console.log("it is a ${req.method} request!")
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const { id } = req.params
+  Users.getById(id)
+  .then(possibleUser => {
+    if (possibleUser) {
+      req.users = possibleUser
+      next()
+    } else {
+      next({ message: "user not found" });
+    }
+  })
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +30,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId }
